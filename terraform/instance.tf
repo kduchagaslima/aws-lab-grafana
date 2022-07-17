4107af2a48bd1dd5359cc31af6e0498e81ec5622
Carlos Eduardo Lima <kduchagas.lima@gmail.com>
2022-07-17 17:27:52 -0300

    Adjust security group

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,6 +1,60 @@
+resource "aws_security_group" "default" {
+  name        = "lab-monitor-ec2-security-group-instance"
+  description = "Grupo de seguranca do EC2"
+
+  # Acesso SSH de qualquer um
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Acesso HTTP de qualquer um
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Acesso HTTPS de qualquer um
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Acesso Grafana de qualquer um
+  ingress {
+    from_port = 3000
+    to_port = 3000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Acesso Prometheus de qualquer um
+  ingress {
+    from_port = 9090
+    to_port = 9090
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  # Acesso de saida para internet
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
   security_groups = ["launch-wizard-1"]
   root_block_device {
     volume_type = "gp2"
