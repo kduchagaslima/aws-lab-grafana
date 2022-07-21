b8220040c18bfb7471d311ae4e344651c234336a
Carlos Eduardo Lima <kduchagas.lima@gmail.com>
2022-07-21 19:46:46 -0300

    update docker images on docker-compose

--- terraform/script/init-setup.sh
+++ terraform/script/init-setup.sh
@@ -28,7 +28,7 @@ instalaDockerCompose () {
 importRepoGrafana () {
     sudo chmod 777 /opt -R
     mkdir /opt/repos
-    pushd /opt/repos && git clone https://github.com/Einsteinish/Docker-Compose-Prometheus-and-Grafana.git && popd
+    pushd /opt/repos && git clone https://github.com/kduchagaslima/docker-compose-prometheus-and-grafana.git && popd
     pushd /opt/repos/Docker-Compose-Prometheus-and-Grafana && docker-compose -f docker-compose.yml up -d
 }
 
